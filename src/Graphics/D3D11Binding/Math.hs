7f70693855dadd5ea8e0bc79c7a264096b743da0
Nam Hyeon Uk <jwvg0425@naver.com>
2016-05-03 00:43:45 +0900

    add matrix module

--- src/Graphics/D3D11Binding/Math.hs
+++ src/Graphics/D3D11Binding/Math.hs
@@ -1,7 +1,5 @@
 module Graphics.D3D11Binding.Math 
-( module V3
-, module V4
+( module Graphics.D3D11Binding.Math.Matrix
 ) where
 
-import qualified Graphics.D3D11Binding.Math.Vertex3 as V3
-import qualified Graphics.D3D11Binding.Math.Vertex4 as V4
+import Graphics.D3D11Binding.Math.Matrix
---
+++ src/Graphics/D3D11Binding/Math/Matrix.hs
@@ -0,0 +1,24 @@
+module Graphics.D3D11Binding.Math.Matrix where
+  
+import Data.Matrix
+
+import Foreign.Ptr
+import Foreign.Marshal.Array
+import Foreign.Storable
+import Foreign.CStorable
+
+type Matrix4 = Matrix Float
+
+matrix4 = matrix 4 4
+
+matrix4FromList = fromList 4 4
+
+instance (Storable m) => Storable (Matrix m) where
+  sizeOf _ = 64
+  alignment _ = 8
+  peek ptr = do 
+    list <- peekArray 16 (castPtr ptr)
+    return $ matrix4FromList list
+  poke ptr mat = do
+    let list = toList mat
+    pokeArray (castPtr ptr) list
